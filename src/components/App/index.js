5712a576113f557016f0c233105f74c3eae59e5a
Jean-Denis Vauguet <jd@vauguet.fr>
2018-06-26 09:29:54 +0200

    Point de départ (nettoyage)

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,34 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import './app.styl';
 
-/**
- * L'application simule un lancé de dé.
- */
-const App = ({
-  diceFacesNb,
-  randomValue,
-  generateRandomValue,
-}) => (
-  <div>
-    <h1>{`D${diceFacesNb}`}</h1>
-    <p className="message">
-      {randomValue}
-      <br />
-      <button onClick={generateRandomValue}>Lancer le dé !</button>
-    </p>
-  </div>
+const App = () => (
+  <div className="app">Ici, une chatroom… EPAF le chien :D</div>
 );
-
-App.propTypes = {
-  diceFacesNb: PropTypes.number.isRequired,
-  randomValue: PropTypes.number,
-  generateRandomValue: PropTypes.func.isRequired,
-};
-
-App.defaultProps = {
-  randomValue: 0,
-};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import App from '~/containers/App';
+import App from '~/components/App';
 import store from '~/store';
 
-// Ajout de l'élément virtuel dans le DOM du navigateur (mount/render).
-// cible.appendChild(quoiajouter)
 document.addEventListener('DOMContentLoaded', () => {
   const rootComponent = (
     <Provider store={store}>
-      <App diceFacesNb={3} />
+      <App />
     </Provider>
   );
   const targetNode = document.getElementById('root');
