3062fc6f1813df8239a8646a8b282a95f5b8199e
Jean-Denis Vauguet <jd@vauguet.fr>
2018-06-26 10:30:08 +0200

    Démo de socket.io pour un chat minimal

--- src/index.js
+++ src/index.js
@@ -16,3 +16,24 @@ document.addEventListener('DOMContentLoaded', () => {
   render(rootComponent, targetNode);
 });
 
+// const socket = window.io();
+
+// console.log('WebSocket', socket);
+// console.log('---');
+
+// // Fonction qui pourrait être branchée sur un onSubmit
+// const sendMessage = (message) => {
+//   socket.emit('chat message', message);
+// };
+
+// const displayMessage = (message) => {
+//   // On pourrait refresh  du JSX (un composant React)
+//   console.log(message);
+// };
+
+// socket.on('chat message', displayMessage);
+
+// sendMessage('salut !');
+// sendMessage('ça va ?');
+// sendMessage('Yep, et toi ?');
+// sendMessage('Bien.');
